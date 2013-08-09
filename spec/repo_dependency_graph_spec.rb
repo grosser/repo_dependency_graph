524dfff2393e31b8d31c53d52b25065037d03e58
grosser <grosser.michael@gmail.com>
2013-08-08 21:03:54 -0700

    initial

--- spec/repo_dependency_graph_spec.rb
+++ spec/repo_dependency_graph_spec.rb
@@ -4,4 +4,24 @@ describe RepoDependencyGraph do
   it "has a VERSION" do
     RepoDependencyGraph::VERSION.should =~ /^[\.\da-z]+$/
   end
+
+  context "CLI" do
+    it "shows --version" do
+      audit("--version").should include(RepoDependencyGraph::VERSION)
+    end
+
+    it "shows --help" do
+      audit("--help").should include("Draw repo dependency graph from your organization")
+    end
+
+    def audit(command, options={})
+      sh("bin/repo-dependency-graph #{command}", options)
+    end
+
+    def sh(command, options={})
+      result = `#{command} #{"2>&1" unless options[:keep_output]}`
+      raise "FAILED #{command}\n#{result}" if $?.success? == !!options[:fail]
+      result
+    end
+  end
 end
